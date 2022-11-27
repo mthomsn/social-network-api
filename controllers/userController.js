a89bf14da1caa2aa5be22f41d7d72414021e9af7
Matt <matthewthomsen99@gmail.com>
2022-11-27 15:21:44 -0600

    add user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,5 +25,58 @@ module.exports = {
   },
 
   // update user by id
-  
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId},
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+  },
+
+  // delete user and associated thoughts
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) => {
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } });
+      })
+      .then(() => res.json({ message: 'User deleted' }))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // add a friend to a user's friend list
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // remove a friend from a user's friend list
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((user) => 
+        !user
+          ? res.status(404).json({ message: 'No user with' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
